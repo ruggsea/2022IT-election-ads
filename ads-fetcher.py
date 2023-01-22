e02ee4e949abb947b195df9dec3d1965a5571b7b
ruggsea <rmlazza@gmail.com>
2023-01-22 16:01:58 +0100

    First batch of ads screenshots

--- ads-fetcher.py
+++ ads-fetcher.py
@@ -1,43 +1,55 @@
-
-# read every line of ads_ids.txt into a list of ids
-
-
-
+import os
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-from selenium import webdriver 
+from selenium import webdriver
 import uuid
 import time
 options = Options()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
+# options.add_argument('--headless')
+# options.add_argument('--no-sandbox')
 options.add_argument("window-size=1920,1080")
+driver = webdriver.Chrome(options=options)
 
 
-driver=webdriver.Chrome(options=options)
+def get_ads_from_file(file):
 
-for id in ids:
-    url="https://www.facebook.com/ads/archive/render_ad/?id={id}&access_token=EAAVgpvtpZBTcBAM8YCEuDzRZAFJ3NhQkgyaPjm0c5QB3TtY7hnyZAZBLzDme4mULrZC8DdiZBbXOzM6eK4oyqWqdXlGZAFC1FXdDMDJemz84S8lwr0zdJzcGUoK5Dza9uZBsTiIoHNTI1G4FTn9XvlNXrLv05KQZBlvpZCmwJ8qHR4YUdNZBLzWOkUNFIM1f5i7YNQZD".format(id=id)
-    driver.get(url)
+    # get token from environmental variable
 
-    time.sleep(5)
+    token = os.environ.get("fb_api_key")
 
-    print(url)
+    # read every line of ads_ids.txt into a list of ids
 
-    ad=driver.find_element(By.CLASS_NAME, "_8n-d")
-    driver.execute_script('arguments[0].scrollIntoView({block: "center"});', ad)
-    ad.screenshot("ads/{id}.png".format(id=id))
+    with open(file=file) as f:
+        ids = f.readlines()
 
-driver.quit()
+    print("The script will try to screenshot {len} ads".format(len=len(ids)))
+
+    # for each id in the list of ids, fetch the ad and save it as a png file
+
+    counter = 0
+    for id in ids:
+        url = "https://www.facebook.com/ads/archive/render_ad/?id={id}&access_token={token}".format(
+            id=id,
+            token=token
+        )
+        driver.get(url)
+        time.sleep(0.3)
+        print(id)
+        ad = driver.find_element(By.CLASS_NAME, "_8n-d")
+        driver.execute_script(
+            'arguments[0].scrollIntoView({block: "center"});', ad)
+        ad.screenshot("ads/{id}.png".format(id=id.strip()))
+        ids.remove(id)
+        if counter == 999:
+            with open('ads_ids.txt', 'w') as f:
+                f.writelines(ids)
+            counter = 0
+        else:
+            counter += 1
+
+    driver.quit()
 
 
-
-
-
-    
-
-
-
-
-
+if __name__ == "__main__":
+    get_ads_from_file("ads_ids.txt")
